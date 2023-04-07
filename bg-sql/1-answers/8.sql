4cd22499de84d8188be0fefa5329794aa05f31ef
Tyler Gao <tgao564@cs.uchicago.edu>
2023-04-07 15:14:27 -0500

    add2

--- bg-sql/1-answers/8.sql
+++ bg-sql/1-answers/8.sql
@@ -2,10 +2,10 @@ SELECT TRIM(name) AS name
 FROM (
   SELECT TRIM(name) AS name
   FROM games
-  WHERE LOWER(name) LIKE '%arc%'
+  WHERE (name) LIKE '%arc%'
   UNION
   SELECT TRIM(d.designer) AS name
   FROM designers d
-  WHERE LOWER(d.designer) LIKE '%arc%'
+  WHERE (d.designer) LIKE '%arc%'
 ) AS combined_names
 ORDER BY name ASC;

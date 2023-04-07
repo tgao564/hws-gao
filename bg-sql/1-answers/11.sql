7685bf9e9c4b7bd4085a53f9f693611e311aa58c
Tyler Gao <tgao564@cs.uchicago.edu>
2023-04-07 14:37:03 -0500

    add2

--- bg-sql/1-answers/11.sql
+++ bg-sql/1-answers/11.sql
@@ -1,7 +1,8 @@
-SELECT g.name AS category, AVG(g.avgscore) AS avg
+SELECT g.name AS category, MAX(g.avgscore) AS avg
 FROM gamecat gc
 JOIN games g ON gc.g_id = g.g_id
 JOIN categories c ON gc.c_id = c.c_id
 GROUP BY g.name
-ORDER BY avg DESC, g.name ASC
+HAVING COUNT(g.g_id) >= 1
+ORDER BY avg DESC, category ASC
 LIMIT 5;

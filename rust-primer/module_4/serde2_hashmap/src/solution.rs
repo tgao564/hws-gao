24989fb8d2bd85430294de9d906260f97b7c50fd
Tyler Gao <tgao564@cs.uchicago.edu>
2023-04-12 11:19:21 -0500

    fix hash

--- rust-primer/module_4/serde2_hashmap/src/solution.rs
+++ rust-primer/module_4/serde2_hashmap/src/solution.rs
@@ -13,9 +13,10 @@ pub fn serialize_data_to_disk(data: &HashMap<String, i32>, filename: &str) -> Re
 }
 
 pub fn deserialize_data_from_disk(filename: &str) -> HashMap<String, i32> {
-    let mut file = File::open(filename).expect("Failed to open file");
+    let file = File::open(filename).expect("Failed to open file");
+    let mut buf_reader = BufReader::new(file);
     let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("Failed to read file");
+    buf_reader.read_to_string(&mut contents).expect("Failed to read file");
     let mut result = HashMap::new();
     for line in contents.lines() {
         let tokens: Vec<&str> = line.split_whitespace().collect();
